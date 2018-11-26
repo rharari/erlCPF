1e9864a50ec529f2cc21d9a8a8447b058020bf09
Mateus Manuel <mateusmanuel@unb.br>
2018-11-26 18:47:39 -0200

    Fix special case of reminder 10

--- cpf.erl
+++ cpf.erl
@@ -6,9 +6,9 @@
 
 isValid(S) ->
 	C = normalizar(lists:filter(fun(X) -> X>47 andalso X<58 end, S)),
-	D = lists:sum( lists:zipwith(fun(X,Y) -> (X-48)*Y end, C, [1,2,3,4,5,6,7,8,9,0,0]) ) rem 11,
+	D = ( lists:sum( lists:zipwith(fun(X,Y) -> (X-48)*Y end, C, [1,2,3,4,5,6,7,8,9,0,0]) ) rem 11 ) rem 10,
 	D =:= lists:nth(10, C) - 48 andalso 
-		( lists:sum(lists:zipwith(fun(X,Y) -> (X-48)*Y end, C, [0,1,2,3,4,5,6,7,8,0,0]) ) + D * 9 ) rem 11 =:= lists:nth(11, C) - 48.
+		( ( lists:sum(lists:zipwith(fun(X,Y) -> (X-48)*Y end, C, [0,1,2,3,4,5,6,7,8,0,0]) ) + D * 9 ) rem 11 ) rem 10 =:= lists:nth(11, C) - 48.
 		
 
 %% Internal functions
